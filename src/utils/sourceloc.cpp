880044913f4cc8d1dafdde3210d89241c53a3d82
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-02-06 00:30:45 +0530

    add errorlogger

---
+++ src/utils/sourceloc.cpp
@@ -0,0 +1,17 @@
+#include "sourceloc.h"
+
+using namespace Crust;
+
+void SourceLocation::init() {
+    mCurrentColumn = 1;
+    mCurrentLine = 1;
+}
+
+void SourceLocation::advance(bool nextLine) {
+    if (nextLine) {
+        ++mCurrentLine;
+        mCurrentColumn = 1;
+    } else {
+        ++mCurrentColumn;
+    }
+}
