d11c4b226086b17eab87bd3de4335578c78a7448
Aditya Chopra <adeecc11@gmail.com>
2022-02-09 22:18:22 +0530

    Add fully function untested Lexer (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,37 @@
 #include "lexer/lexer.hpp"
 
 int main() {
-    std::cout << "Enter Filename: ";
-    std::string filename;
-    std::cin >> filename;
-
     Crust::Lexer lexer;
-    lexer.init(filename);
+    lexer.init("test.crst");
 
     Crust::Lexer::Token currentToken;
     currentToken = lexer.getNextToken();
 
     while (currentToken != Crust::Lexer::Token::TOK_EOF) {
-        std::cout << "[" << lexer.GetCurrentLocation().getCurrentLine() << "]" << (int)currentToken << std::endl;
+        std::cout << "[" << lexer.GetCurrentLocation().getCurrentLine() << ":" << lexer.GetCurrentLocation().getCurrentColumn() << "] Token ID: " << (int)currentToken;
+
+        switch (currentToken) {
+            case Crust::Lexer::Token::IDENTIFIER:
+                std::cout << " Identifier Name: " << lexer.getCurrentStr() << std::endl;
+                break;
+            case Crust::Lexer::Token::STR_LITERAL:
+                std::cout << " String Literal: " << lexer.getCurrentStr() << std::endl;
+                break;
+
+            case Crust::Lexer::Token::INT_LITERAL:
+                std::cout << " Int Literal: " << lexer.getCurrentInt() << std::endl;
+                break;
+
+            case Crust::Lexer::Token::FLOAT_LITERAL:
+                std::cout << " Float Literal: " << lexer.getCurrentFloat() << std::endl;
+                break;
+
+            default:
+                std::cout << std::endl;
+
+                break;
+        }
+
         currentToken = lexer.getNextToken();
     }
 
